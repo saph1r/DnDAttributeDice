5cc7ebf9873a2f11be9d89233857ff2337a30129
sdonath <sdonaq5@gmail.com>
2017-04-13 17:12:01 +0200

    Update RollAllDice

--- app/src/main/java/com/example/sdonath/dndattributedice/AttributeDice.java
+++ app/src/main/java/com/example/sdonath/dndattributedice/AttributeDice.java
@@ -1,16 +1,19 @@
 package com.example.sdonath.dndattributedice;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class AttributeDice extends AppCompatActivity {
@@ -22,6 +25,8 @@ public class AttributeDice extends AppCompatActivity {
     private int[][] dice = new int[7][4];
     private Random rand;
 
+    private ArrayList<ImageView> diceImageViews = new ArrayList<ImageView>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +34,14 @@ public class AttributeDice extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         //setze alle scores auf 0
     /*for(int i = 0; i<7;i++){
@@ -60,17 +65,134 @@ public class AttributeDice extends AppCompatActivity {
 
         rand = new Random();
 
+        ImageView die11Image = (ImageView) findViewById(R.id.die11Image);
+        ImageView die12Image = (ImageView) findViewById(R.id.die12Image);
+        ImageView die13Image = (ImageView) findViewById(R.id.die13Image);
+        ImageView die14Image = (ImageView) findViewById(R.id.die14Image);
+        ImageView die21Image = (ImageView) findViewById(R.id.die21Image);
+        ImageView die22Image = (ImageView) findViewById(R.id.die22Image);
+        ImageView die23Image = (ImageView) findViewById(R.id.die23Image);
+        ImageView die24Image = (ImageView) findViewById(R.id.die24Image);
+        ImageView die31Image = (ImageView) findViewById(R.id.die31Image);
+        ImageView die32Image = (ImageView) findViewById(R.id.die32Image);
+        ImageView die33Image = (ImageView) findViewById(R.id.die33Image);
+        ImageView die34Image = (ImageView) findViewById(R.id.die34Image);
+        ImageView die41Image = (ImageView) findViewById(R.id.die41Image);
+        ImageView die42Image = (ImageView) findViewById(R.id.die42Image);
+        ImageView die43Image = (ImageView) findViewById(R.id.die43Image);
+        ImageView die44Image = (ImageView) findViewById(R.id.die44Image);
+        ImageView die51Image = (ImageView) findViewById(R.id.die51Image);
+        ImageView die52Image = (ImageView) findViewById(R.id.die52Image);
+        ImageView die53Image = (ImageView) findViewById(R.id.die53Image);
+        ImageView die54Image = (ImageView) findViewById(R.id.die54Image);
+        ImageView die61Image = (ImageView) findViewById(R.id.die61Image);
+        ImageView die62Image = (ImageView) findViewById(R.id.die62Image);
+        ImageView die63Image = (ImageView) findViewById(R.id.die63Image);
+        ImageView die64Image = (ImageView) findViewById(R.id.die64Image);
+        ImageView die71Image = (ImageView) findViewById(R.id.die71Image);
+        ImageView die72Image = (ImageView) findViewById(R.id.die72Image);
+        ImageView die73Image = (ImageView) findViewById(R.id.die73Image);
+        ImageView die74Image = (ImageView) findViewById(R.id.die74Image);
+
+        diceImageViews.add(die11Image);
+        diceImageViews.add(die12Image);
+        diceImageViews.add(die13Image);
+        diceImageViews.add(die14Image);
+        diceImageViews.add(die21Image);
+        diceImageViews.add(die22Image);
+        diceImageViews.add(die23Image);
+        diceImageViews.add(die24Image);
+        diceImageViews.add(die31Image);
+        diceImageViews.add(die32Image);
+        diceImageViews.add(die33Image);
+        diceImageViews.add(die34Image);
+        diceImageViews.add(die41Image);
+        diceImageViews.add(die42Image);
+        diceImageViews.add(die43Image);
+        diceImageViews.add(die44Image);
+        diceImageViews.add(die51Image);
+        diceImageViews.add(die52Image);
+        diceImageViews.add(die53Image);
+        diceImageViews.add(die54Image);
+        diceImageViews.add(die61Image);
+        diceImageViews.add(die62Image);
+        diceImageViews.add(die63Image);
+        diceImageViews.add(die64Image);
+        diceImageViews.add(die71Image);
+        diceImageViews.add(die72Image);
+        diceImageViews.add(die73Image);
+        diceImageViews.add(die74Image);
     }
     public int roll(Random r){
         return r.nextInt(6)+1;
     }
     public void rollDice(View v){
         int buttonTag = Integer.parseInt(v.getTag().toString());
-        dice[buttonTag-1][0] = roll(rand);
-        dice[buttonTag-1][1] = roll(rand);
-        dice[buttonTag-1][2] = roll(rand);
-        dice[buttonTag-1][3] = roll(rand);
-        int diceResultInt = dice[buttonTag-1][0] + dice[buttonTag-1][1] + dice[buttonTag-1][2] + dice[buttonTag-1][3];
+        int diceResultInt =0;
+        int lowestRollScore = 24;
+        int lowestRollNum=1;
+        for (int i = 0; i<4;i++){
+            dice[buttonTag-1][i] = roll(rand);
+            diceResultInt += dice[buttonTag-1][i];
+            String imageName = "die_" + dice[buttonTag-1][i] + ".png";
+            try{
+                InputStream stream = getAssets().open(imageName);
+                Drawable d = Drawable.createFromStream(stream,null);
+                //die liste ist 4*7 28 felder groß, mit geti bekomme ich felder 0-3, aber nicht felder 4-27
+                if(buttonTag == 1)
+                    diceImageViews.get(i).setImageDrawable(d);
+                else if(buttonTag==2)
+                    diceImageViews.get(i+4).setImageDrawable(d);
+                else if(buttonTag==3)
+                    diceImageViews.get(i+8).setImageDrawable(d);
+                else if(buttonTag==4)
+                    diceImageViews.get(i+12).setImageDrawable(d);
+                else if(buttonTag==5)
+                    diceImageViews.get(i+16).setImageDrawable(d);
+                else if(buttonTag==6)
+                    diceImageViews.get(i+20).setImageDrawable(d);
+                else if(buttonTag==7)
+                    diceImageViews.get(i+24).setImageDrawable(d);
+                stream.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            int temp = dice[buttonTag-1][i];
+            if(lowestRollScore>temp){
+                lowestRollScore = temp;
+                lowestRollNum = i;
+            }
+        }
+        //lowestResult abziehen
+        diceResultInt -=lowestRollScore;
+        String imageNameF = "die_" + dice[buttonTag-1][lowestRollNum]+"f.png";
+        try{
+            InputStream stream = getAssets().open(imageNameF);
+            Drawable d = Drawable.createFromStream(stream,null);
+            //die liste ist 4*7 28 felder groß, mit geti bekomme ich felder 0-3, aber nicht felder 4-27
+            if(buttonTag == 1)
+                diceImageViews.get(lowestRollNum).setImageDrawable(d);
+            else if(buttonTag==2)
+                diceImageViews.get(lowestRollNum+4).setImageDrawable(d);
+            else if(buttonTag==3)
+                diceImageViews.get(lowestRollNum+8).setImageDrawable(d);
+            else if(buttonTag==4)
+                diceImageViews.get(lowestRollNum+12).setImageDrawable(d);
+            else if(buttonTag==5)
+                diceImageViews.get(lowestRollNum+16).setImageDrawable(d);
+            else if(buttonTag==6)
+                diceImageViews.get(lowestRollNum+20).setImageDrawable(d);
+            else if(buttonTag==7)
+                diceImageViews.get(lowestRollNum+24).setImageDrawable(d);
+            stream.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+        //int diceResultInt = dice[buttonTag-1][0] + dice[buttonTag-1][1] + dice[buttonTag-1][2] + dice[buttonTag-1][3];
         String diceResultString = Integer.toString(diceResultInt);
         scores[buttonTag-1] =diceResultInt;
         rollResults[buttonTag-1].setText(diceResultString);
@@ -78,9 +200,8 @@ public class AttributeDice extends AppCompatActivity {
     }
     public void rollAllDice(View v){
         for(int i = 0; i<7; i++){ //gehe von 0 bis 6
-            scores[i] = rand.nextInt(6)+1; //score hat die größe 0 bis 6
-            String RandomValue = Integer.toString(scores[i]);
-            rollResults[i].setText(RandomValue);
+            v.setTag(i+1);
+            rollDice(v);
         }
     }
 
